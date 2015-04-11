c63ece5bc4f6bfa29671cb91eb12ceea4da57e0a
raymnondj <jsnray7@gmail.com>
2015-04-11 10:24:53 -0400

    Added Exercise 1

---
+++ ex1.rb
@@ -0,0 +1,7 @@
+puts "Hello World!" #put string method. Writes a new line after the object.
+puts "Hello Again!" #put string method. Writes a new line after the object.
+puts "I like typing like this." #put string method. Writes a new line after the object.
+puts "This is fun." #put string method. Writes a new line after the object
+puts "Yay!" #put string method. Writes a new line after the object, in this case a string.
+puts "I'd much rather you 'not'" #put string method. Anything inside single quotes will not execute string interpolation.
+puts 'I said "don\'t" touch this.'#put string method.  Anything inside double quotes allows string interpolation. \' allows insertion of a single quote which Ruby will leave alone.
