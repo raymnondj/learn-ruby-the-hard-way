d26a6352f9334a4dd6824c0b019adb0716b85f1c
raymnondj <jsnray7@gmail.com>
2015-04-13 07:37:01 -0400

    Added Exercise 2

---
+++ ex2.rb
@@ -0,0 +1,8 @@
+#A comment following a hashtag, this is ignored by Ruby
+#Anything on the same line after the # is ignored
+
+puts "I could have code like this." #and the comment after is ignored
+
+#You can also use comments to 'disable' code
+#puts "This won't run"
+puts "This will run."
