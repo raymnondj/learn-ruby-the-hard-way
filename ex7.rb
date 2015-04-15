7a4af40d9f43a0129fcd9f863b484ba3f796d3e6
raymnondj <jsnray7@gmail.com>
2015-04-15 08:41:40 -0400

    Added Exercise 7

---
+++ ex7.rb
@@ -0,0 +1,21 @@
+puts "Mary had a little lamb" #put string
+puts "It's fleece was white as #{'snow'}" #put string the #{} in this case don't really do anything
+puts "And everywhere that Mary went." #put string
+puts "." * 10 #this will put the string 10 times
+
+end1 = "C" #sets value of variable end1
+end2 = "h" #see above
+end3 = "e" #see above
+end4 = "e" #see above
+end5 = "s" #see above
+end6 = "e" #see above
+end7 = "b" #see above
+end8 = "u" #see above
+end9 = "r" #see above
+end10 = "g" #see above
+end11 = "e" #see above
+end12 = "r" #see above
+
+print end1 + end2 + end3 + end4 + end5 + end6 #this will concatenate the strings, and will not create a new line character
+puts end7 + end8 +end9 + end10 + end11 +end12 #concatenation. The word "burger" will appear directly after the above string
+
