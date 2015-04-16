97b2e65bdb9948287b8247662f526ba00efcd049
raymnondj <jsnray7@gmail.com>
2015-04-16 11:32:26 -0400

    Added Exercise 10

---
+++ ex10.rb
@@ -0,0 +1,15 @@
+tabby_cat = "\tI'm tabbed in."  #sets the string as a variable tabby_cat.  the \t code will tab the string in
+persian_cat = "I'm split \non a line." #sets the string as a variable persian_cat. the \n code will create a new line
+backslash_cat = "I'm \\ a \\ cat." #sets the string as a variable backslash_cat.  the \\ code will insert a backslash
+
+fat_cat = """ 
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+""" #the triple quotes will create a multi-line string.  \t and \n code is described above
+
+puts tabby_cat #put string
+puts persian_cat #put string
+puts backslash_cat #put string
+puts fat_cat #put string
