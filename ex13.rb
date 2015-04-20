ad0a11cf3bc109346e0ded0d19c61631885c6688
raymnondj <jsnray7@gmail.com>
2015-04-20 13:44:51 -0400

    Added Exercise 13

---
+++ ex13.rb
@@ -0,0 +1,6 @@
+first, second, third = ARGV #ARGV is an argument varibale. Allows users to provide arguments in command line when executing the program
+
+puts "Your first variable is #{first}" #this will put the first argument
+puts "Your second variable is #{second}" #this will put the second
+puts "Your third variable is #{third}" #this will put the third
+
